0670412f9898e5ffeebca085c61a97d7edaa6f44
alonewalker365 <alonewalker3125@gmail.com>
2025-01-28 17:58:55 +0530

    initialized next

---
+++ app/page.js
@@ -0,0 +1,10 @@
+
+import "../app/globals.css"
+import LandingComponent from "@/components/LandingComponent/LandingComponent";
+
+
+export default function Home() {
+  return (
+    <LandingComponent/>
+  );
+}
